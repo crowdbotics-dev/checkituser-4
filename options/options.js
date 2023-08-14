3238a517912d89df1c512d11cacf2fe5931e9c2a
Crowdbotics <engineering@crowdbotics.com>
2023-08-14 07:16:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "checkituser-4",
+  url: "https://checkituser-4.botics.co",
+  api: "https://checkituser-4.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
